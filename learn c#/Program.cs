2f401661bf5041090e02acced73f6f0edef4bae7
dato <davidmoogymoogy@gmail.com>
2026-05-26 08:57:45 -0700

    update 3 tavi

--- learn c#/Program.cs
+++ learn c#/Program.cs
@@ -5,77 +5,243 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("hello world");
-        Console.WriteLine(1);
+        //Console.WriteLine("hello world");
+        //Console.WriteLine(1);
 
-        Console.WriteLine(3+5);
-        Console.WriteLine(5-3);
+        //Console.WriteLine(3+5);
+        //Console.WriteLine(5-3);
 
-        void Print(string name) {
-            Console.WriteLine("Enter your name : ");
-            name = Console.ReadLine();
-            Console.WriteLine("hello " + name);
-        }
-        Print("dato");
+        //void Print(string name) {
+        //    Console.WriteLine("Enter your name : ");
+        //    name = Console.ReadLine();
+        //    Console.WriteLine("hello " + name);
+        //}
+        //Print("dato");
 
-        string textt = "abcdferrf";
-        Console.WriteLine(textt.Length);
+        //string textt = "abcdferrf";
+        //Console.WriteLine(textt.Length);
 
-        string strqion1 = "good morning";
-        string striqoni2 = "good evenig";
+        //string strqion1 = "good morning";
+        //string striqoni2 = "good evenig";
 
-        string merged = strqion1 + " " +  striqoni2;
+        //string merged = strqion1 + " " +  striqoni2;
 
-        string lowercase = strqion1.ToLower();
-        string upercase = striqoni2.ToUpper();
+        //string lowercase = strqion1.ToLower();
+        //string upercase = striqoni2.ToUpper();
 
-        Console.WriteLine(merged);
-        Console.WriteLine(lowercase);
-        Console.WriteLine(upercase);
-
-
-        Console.WriteLine("Enter yout username : ");
-        string username = Console.ReadLine();
-
-        Console.WriteLine(username.ToLower(), username.ToUpper());
+        //Console.WriteLine(merged);
+        //Console.WriteLine(lowercase);
+        //Console.WriteLine(upercase);
 
 
-        Console.WriteLine("Enter text something t oshow lenght of your text and get four symbols ");
-        string Textt = Console.ReadLine();
-        Console.WriteLine(Textt.Length);
-        Console.WriteLine(Textt.Substring(0, 4));
+        //Console.WriteLine("Enter yout username : ");
+        //string username = Console.ReadLine();
 
-        int a = 50;
-        int b = 100;
-        Console.WriteLine(a);
-        Console.WriteLine(b);
+        //Console.WriteLine(username.ToLower(), username.ToUpper());
 
-        double num1 = 12.34;
-        double num2 = 24.13;
-        double sum = num1 * num2;
-        Console.WriteLine(sum);
 
-        char symbol = 'A';
+        //Console.WriteLine("Enter text something t oshow lenght of your text and get four symbols ");
+        //string Textt = Console.ReadLine();
+        //Console.WriteLine(Textt.Length);
+        //Console.WriteLine(Textt.Substring(0, 4));
 
-        Console.WriteLine(char.IsDigit(symbol));
+        //int a = 50;
+        //int b = 100;
+        //Console.WriteLine(a);
+        //Console.WriteLine(b);
 
-        bool online = true;
-        Console.WriteLine(!online);
+        //double num1 = 12.34;
+        //double num2 = 24.13;
+        //double sum = num1 * num2;
+        //Console.WriteLine(sum);
 
-        Console.WriteLine("sheiyvanet winadadeba : ");
-        string winadadeba = Console.ReadLine();
-        Console.WriteLine("Sheiyvanot sityva : ");
-        string sityva = Console.ReadLine();
+        //char symbol = 'A';
 
-        Console.WriteLine(winadadeba.Contains(sityva));
+        //Console.WriteLine(char.IsDigit(symbol));
 
-        string updatedString = winadadeba.Replace(" ", "-");
-        Console.WriteLine(updatedString);
+        //bool online = true;
+        //Console.WriteLine(!online);
 
-        double ricxvi = 4;
-        double root = Math.Sqrt(ricxvi);
+        //Console.WriteLine("sheiyvanet winadadeba : ");
+        //string winadadeba = Console.ReadLine();
+        //Console.WriteLine("Sheiyvanot sityva : ");
+        //string sityva = Console.ReadLine();
 
-        double gaormagebuli = ricxvi * 2;
+        //Console.WriteLine(winadadeba.Contains(sityva));
+
+        //string updatedString = winadadeba.Replace(" ", "-");
+        //Console.WriteLine(updatedString);
+
+        //double ricxvi = 4;
+        //double root = Math.Sqrt(ricxvi);
+
+        //double gaormagebuli = ricxvi * 2;
+
+
+        //if else davalebebi 
+
+
+        //Console.WriteLine("Enter number ");
+
+        //int num = int.Parse(Console.ReadLine());
+
+
+        //if (num % 2 == 0) {
+        //    Console.WriteLine("luwia");
+        //}
+        //else {
+        //    Console.WriteLine("kenti");
+        //}
+
+        //if(num > 0)
+        //{
+        //    Console.WriteLine("dadebiti");
+        //}
+        //else if(num < 0)
+        //{
+        //    Console.WriteLine("uaryofiti");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("ricxvi aris 0");
+        //}
+
+
+        //int secondnum = 20;
+
+        //if(num % 2 == secondnum)
+        //{
+        //    Console.WriteLine("iyofa tavisuflad");
+        //}
+        //else
+        //{
+        //     Console.WriteLine("ar iyofa tavisuflad");
+
+        //}
+
+
+        //string teqsti = "hello world";
+
+        //if(teqsti.Length == 0)
+        //{
+        //    Console.WriteLine("carielia");
+        //}
+        //else        {
+        //    Console.WriteLine("carieli ar  aris " + "sigrdze aris " + teqsti.Length);
+        //}
+
+        //if (teqsti.Contains("world"))
+        //{
+        //    Console.WriteLine("sheicavs");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("ar sheicavs");
+        //}
+
+
+        //int a = 24;
+        //int b = 12;
+
+        //if(a > b)
+        //{
+        //    Console.WriteLine(a + "metia");
+        //}
+        //else
+        //{
+        //    Console.WriteLine(b+ "metia");
+        //}
+
+        //if(a< b)
+        //{
+        //    Console.WriteLine(a + "naklebia");
+        //}
+        //else
+        //{
+        //    Console.WriteLine(b + "naklebia");
+        //}
+
+
+        //Console.WriteLine("Enter age : ");
+
+        //int age = int.Parse(Console.ReadLine());
+
+        //if(age >= 18)
+        //{
+        //    Console.WriteLine("you are an adult");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("you are  minor");
+        //}
+
+        //Console.WriteLine("Enter your grade");
+        //int grade = int.Parse(Console.ReadLine());
+
+        //if(grade >=91 && grade <= 100)
+        //{
+        //    Console.WriteLine("A");
+        //}
+        //else if(grade >= 81 && grade < 90)
+        //{
+        //    Console.WriteLine("B");
+        //}
+        //else if(grade >= 71 && grade < 80)
+        //{
+        //    Console.WriteLine("C");
+        //}
+        //else if(grade >= 61 && grade < 70)
+        //{
+        //    Console.WriteLine("D");
+        //}
+        //else if(grade >= 0 && grade < 61)
+        //{
+        //    Console.WriteLine("F");
+        //}
+        // else
+        // {
+        //     Console.WriteLine("invalid grade");
+        // }
+
+
+        //Console.Write("Enter a grade (A, B, C, D, or F): ");
+        //string grade = Console.ReadLine();
+
+        //if (grade == "A")
+        //{
+        //    Console.WriteLine("Excellent!");
+        //}
+        //else if (grade == "B")
+        //{
+        //    Console.WriteLine("Good job!");
+        //}
+        //else if (grade == "C")
+        //{
+        //    Console.WriteLine("Satisfactory.");
+        //}
+        //else if (grade == "D")
+        //{
+        //    Console.WriteLine("Needs improvement.");
+        //}
+        //else if (grade == "F")
+        //{
+        //    Console.WriteLine("Failed.");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Invalid grade!");
+        //}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
